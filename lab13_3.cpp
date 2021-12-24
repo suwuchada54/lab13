26e482094867e7c31bd04be724cb91624264f404
suwichada <jippee.1211@gmail.com>
2021-12-24 13:40:43 +0700

    lab13_3.cpp

--- lab13_3.cpp
+++ lab13_3.cpp
@@ -2,7 +2,8 @@
 
 using namespace std;
 
-void mySwap(int &,int &); 
+void mySwap(int &,int &);
+
 
 int main(){
 	int x,y;
@@ -14,4 +15,10 @@ int main(){
 	cout << "x = " << x << ", y = " << y << "\n";
 	
 	return 0;
+}
+
+void mySwap(int &x,int&y){
+	int c=x;
+	x=y;
+	y=c;
 }
